4f94f93d1c0a1ab08c4ba5aa6602bd871d4e0f9e
Fabian von Tiedemann <fabianvontiedemann@AirsomtorFabian.lan>
2025-08-27 12:24:34 +0200

    Add automatic image classes via JavaScript for better CSS targeting

--- assets/image-viewer.js
+++ assets/image-viewer.js
@@ -12,6 +12,9 @@ class ImageViewer {
   }
 
   init() {
+    // Lägg till klasser på alla bilder för styling
+    this.addImageClasses();
+    
     // Skapa overlay HTML
     this.createOverlay();
     
@@ -20,6 +23,22 @@ class ImageViewer {
     
     // Lägg till keyboard navigation
     this.setupKeyboardNavigation();
+  }
+
+  addImageClasses() {
+    // Hitta alla bilder i artiklar och lägg till klasser
+    const articleImages = document.querySelectorAll('main article img');
+    
+    articleImages.forEach((img) => {
+      // Lägg till klasser för styling
+      img.classList.add('article-image');
+      
+      // Lägg till klasser på parent p-tag om den finns
+      const parentP = img.closest('p');
+      if (parentP) {
+        parentP.classList.add('image-container');
+      }
+    });
   }
 
   createOverlay() {
